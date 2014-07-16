cbcfe99b60d8bc638dccfeebdc7ff65f2309e3a5
Alberto Amaro <alberto.aalexander95@gmail.com>
2014-07-16 11:54:21 -0430

    Script para realizar respaldo de un directorio.

--- script/script.sh
+++ script/script.sh
@@ -3,9 +3,11 @@ fecha=$(date +"%m-%d-%Y-%T")
 cd /home/academia/
 #mkdir respaldo
 cd respaldo
-cp -R /home/academia/control/ /home/academia/respaldo/respaldo$fecha
-cd /home/academia/control/canaima/
-git add .
-git commit -a
-git push
+cp -R /home/academia/control/ /home/academia/respaldo/respaldo-$fecha
+cd /home/
+pwd
+cd /home/academia/respaldo/
+#git add .
+#git commit -a
+#git push
 
